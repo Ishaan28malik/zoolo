4bbfbc74c7d205260e36a5eac678a0c684d7f5c4
PythoShark(#techno utopian) <rmalik481@gmail.com>
2018-11-01 00:34:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
-
 // Imports dependencies and set up http server
+
 const
   express = require('express'),
   bodyParser = require('body-parser'),
@@ -16,7 +16,6 @@ app.post('/webhook', (req, res) => {
 
   // Checks this is an event from a page subscription
   if (body.object === 'page') {
-
     // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
@@ -32,9 +31,7 @@ app.post('/webhook', (req, res) => {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
-
 });
-
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
@@ -45,7 +42,7 @@ app.get('/webhook', (req, res) => {
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-
+  
   // Checks if a token and mode is in the query string of the request
   if (mode && token) {
 
@@ -55,7 +52,6 @@ app.get('/webhook', (req, res) => {
       // Responds with the challenge token from the request
       console.log('WEBHOOK_VERIFIED');
       res.status(200).send(challenge);
-
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
